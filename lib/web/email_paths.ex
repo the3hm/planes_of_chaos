175530fef69a7b8c11830a9de3bb3a0b5a7bbaa3
the3hm <the3hm@gmail.com>
2025-04-12 04:59:25 -0400

    abandoned use of sigils, they seem to not work

--- lib/web/email_paths.ex
+++ lib/web/email_paths.ex
@@ -3,11 +3,19 @@ defmodule Web.EmailPaths do
 
   import Web.VerifiedRoutes
 
+  @doc """
+  Generates a full email confirmation URL without using ~p sigil.
+  """
   def confirm_url(token) do
-    Web.Endpoint.url() <> ~p"/users/confirm/#{token}"
+    path = Web.VerifiedRoutes.path(Web.Endpoint, Web.Router, "/users/confirm/#{token}")
+    Web.Endpoint.url() <> path
   end
 
+  @doc """
+  Generates a full password reset verification URL without using ~p sigil.
+  """
   def reset_url(token) do
-    Web.Endpoint.url() <> ~p"/register/reset/verify/#{token}"
+    path = Web.VerifiedRoutes.path(Web.Endpoint, Web.Router, "/register/reset/verify/#{token}")
+    Web.Endpoint.url() <> path
   end
 end
